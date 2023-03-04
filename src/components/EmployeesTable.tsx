a8bdf92707ced3b5e6850f20d9f601094ebe8f3e
Lena Ka <lena.krol@gmail.com>
2023-03-04 13:40:52 +0100

    data.employeesList 'undefined' dont know why.

---
+++ src/components/EmployeesTable.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {EmployeeEntity} from 'types';
+import {EmployeeTableRow} from "./EmployeeTableRow";
+
+
+interface Props {
+    employees: EmployeeEntity[];
+}
+
+export const EmployeesTable = (props: Props) => {
+        return (<table>
+            <thead>
+            <tr>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>E-mail</th>
+                <th>Telefon</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                props.employees.map(employee => (
+                    <EmployeeTableRow employee={employee} key={employee.id}/>
+                ))
+            }
+            </tbody>
+        </table>
+    )
+}
